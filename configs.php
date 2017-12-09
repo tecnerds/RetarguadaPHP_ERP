44c2625e54c6694b45e272879514b30c119a7dda
weslleycold <weslley.capelari@gmail.com>
2017-12-09 01:02:58 -0200

    Create configs.php

---
+++ configs.php
@@ -0,0 +1,9 @@
+<?php
+
+// ---------- Configurações do MySQL
+$mysql_user = ""; // ------ Nome do usuário para entrar no MySQL
+$mysql_password = ""; // ------ Senha para o usuário acima conseguir entrar no MySQL
+$mysql_host = ""; // ------- Endereço onde o servidor MySQL está hospedado
+$mysql_database = ""; // ------ Nome do Banco de Dados do MySQL
+
+?>
