57bd7d8fc9049c45f2ac57214c9999210007238b
tecnerds <patrick.eu.souza@email.com>
2017-12-10 02:43:17 -0200

    Realizado a conexão com BD

---
+++ Banco.php
@@ -0,0 +1,34 @@
+<?php
+
+abstract class Banco{
+    
+    public $servidor = "localhost";
+    public $usuario  = "root";
+    public $senha = "";
+    public $nomebanco = "erp";
+    public $conexao = null;
+    public $dataset = null;
+    public $linhasafetadas = -1;
+    //Construtor
+    public function __construct() {
+        $this->conecta();
+    }
+    //Destrutor
+    public function __destruct() {
+        if($this->conexao != NULL):
+        mysqli_close($this->conexao);
+    endif;
+    }
+    
+    public function conecta(){
+            $this->conexao = mysqli_connect($this->servidor, $this->usuario, $this->senha, $this->nomebanco)or die ("Erro de conexão com servidor ou usuário ou senha");
+            
+            echo "conexão realizado com sucesso!";
+        
+    }
+}
+ ?>
+
+
+
+ 
