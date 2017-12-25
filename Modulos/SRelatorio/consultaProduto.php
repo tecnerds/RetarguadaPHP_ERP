effd00f27483c254a81312fb0c833ea73bbf94db
neyelson <neyelson@gmail.com>
2017-12-25 18:18:51 -0200

    Implementação do front end e reorganização de arquivos

---
+++ Modulos/SRelatorio/consultaProduto.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once '../../Modulos/Database/Banco.php';
+
+	$sql = "select * from produto;";
+	$salva = mysqli_query($conexao, $sql);
+    //começar com 1
+    $parimpar = 0;
+
+	while ($resultado = mysqli_fetch_array($salva)) {
+
+		$id = $resultado[0];
+	    $nome = $resultado[1];
+	    $codBarra = $resultado[2];
+	    $descricao = $resultado[3];
+	    $vCusto = $resultado[4];
+	    $vVenda = $resultado[5];
+
+        //começará com 0 e vai evoluindo, isso também pode registrar quantas resultados tem a tabela
+        $parimpar++;
+        //A tabela do sbadmin precisa marcar com par ou ímpar cada linha
+        if($id % 2 == 0){
+           $parimpar="odd";
+        } else {
+           $parimpar="even";
+        }
+        	    
+	    //echo"<tr><th>$id</th><th>$nome</th><th>$codBarra</th><th>$descricao</th><th>$vCusto</th><th>$vVenda</th></tr>";
+
+        echo'
+
+        <tr class="'.$parimpar.' gradeX">
+        <td>'.$nome.'</td>
+        <td>'.$codBarra.'</td>
+        <td>'.$descricao.'</td>
+        <td class="center">'.$vCusto.'</td>
+        <td class="center">'.$vVenda.'</td>
+        </tr>
+
+        ';
+
+	    }
+
+mysqli_close($conexao);
+
+?>
