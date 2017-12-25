21b7a333e3a60a3b9549240eb04ab6594545ee4d
Neyelson Alves <neyelson@gmail.com>
2017-12-25 17:54:20 -0200

    Delete config.classes.php

--- config.classes.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ------- Cria a classe Funcionario
-class funcionario {
-    public $func_name; // ------ Nome Real do funcionario
-    public $func_userlogin; // ------ Login para entrar no sistema
-    public $func_access; // ------- Nivel de acesso do funcionario as funções
-    public $func_setor; // ------- Setor em que o funcionário trabalha
-}   
-?>
